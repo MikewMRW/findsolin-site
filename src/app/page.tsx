83794271974793d7044f8c3b4535df1b59b6d834
MikewMRW <Mikewmrw@gmail.com>
2025-10-09 20:19:28 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -274,7 +274,7 @@ function Landing() {
   <button
   aria-label="(Hidden) letter H"
   onClick={() => markFound('H')}
-  className={`relative inline-block -ml-[0.42ch] -mr-[0.10ch] align-[-0.38em] rotate-[-6deg] ${
+  className={`relative inline-block -ml-[0.42ch] -mr-[0.18ch] align-[-0.45em] rotate-[-6deg] ${
     debug ? 'outline outline-1 outline-fuchsia-500' : ''
   }`}
   title={debug ? 'Hidden H' : undefined}
